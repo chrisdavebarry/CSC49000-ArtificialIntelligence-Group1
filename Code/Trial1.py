1c67758aa673aa2cc7a471e14f933e0bcc74fac3
Chris Barry <58043829+chrisdavebarry@users.noreply.github.com>
2022-11-09 09:43:31 -0600

    Update Trial1.py

--- Code/Trial1.py
+++ Code/Trial1.py
@@ -1,5 +1,5 @@
 # First import the RobotGenerator
-from mistyPy.Robot import RobotRob
+import mistyPy
 
 # Creating a new robot generator will rewrite the RobotCommands.py and Websocket.py 
 # files to adjust them to the commands and websockets the robot has available
