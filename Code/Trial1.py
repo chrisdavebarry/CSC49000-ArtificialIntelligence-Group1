d7a48e051e88118f14f5247d81f4b28967290c6a
Chris Barry <58043829+chrisdavebarry@users.noreply.github.com>
2022-11-14 13:53:14 -0600

    Update File Structure

--- Code/Trial1.py
+++
@@ -1,6 +0,0 @@
-# First import the RobotGenerator
-import mistyPy
-
-# Creating a new robot generator will rewrite the RobotCommands.py and Websocket.py 
-# files to adjust them to the commands and websockets the robot has available
-RobotGenerator("10.12.132.107")
